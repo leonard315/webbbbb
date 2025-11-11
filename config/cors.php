fac38f63c25069174b34ca7601f5c71a02e0802a
Leonard <leonard315@github.com>
2025-11-11 12:48:18 +0800

    fix: Add Vercel frontend URL to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
+        'https://webbbbb-nu.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
